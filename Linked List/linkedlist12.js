4dec556c8cd910e4b45a86d1d8ae1bf8c8f9bf69
Tricia Buckenberger <tbuckenberger@gmail.com>
2016-07-13 13:09:28 -0700

    exercises

---
+++ Linked List/linkedlist12.js
@@ -0,0 +1,14 @@
+Write an algorithm to determine if a linkedlist is a palindrome
+
+function Node (data) {
+  this.data = data;
+  this.next = null;
+}
+
+function LinkedList () {
+  this.head = null;
+}
+
+function palindrome (listHead) {
+
+}
